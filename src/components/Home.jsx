5d11ae13dde64a369a72ed2d2e481144126dad1d
EishaSatti <esatti07@gmail.com>
2024-09-18 20:48:53 +0500

    building home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
+import Products from "./Products";
 
 const Home = () => {
   return (
-    <div>
-      
+    <div className="hero">
+      <div class="card bg-dark text-white  border-0">
+        <img
+          src="/assets/bag2.jpg"
+          class="card-img-top"
+          alt="Background image"
+          height="550px"
+        />
+        <div className="card-img-overlay d-flex">
+          <div className="container ">
+            <h5 className=" card-title display-3 fw-bolder mb-0 ">
+              NEW ARRIVALS
+            </h5>
+            <p className="card-text lead fs-2">
+                CHECK OUT ALL THE TRENDS
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
