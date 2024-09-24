4a36b69dadff5ae663097e929cd661e037610a4c
EishaSatti <esatti07@gmail.com>
2024-09-24 20:17:24 +0500

    Add products correctly

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,10 +6,10 @@ const Home = () => {
   return (
     <>
     <Header/>
-    <div className=" my-2 py-2 hero">
-      <div className="card right-0 left-0 text-white border-0">
+    <div className="container-fluid w-full my-2 py-2 hero">
+      <div className="card container-fluid text-white border-3 w-full">
         <img src="/assets/bag7.jpg" className="card-img w-full h-[650px]" alt="background image"/>
-        <div className="card-img-overlay">
+        <div className="card-img-overlay w-full container-fluid">
         
           <p className="text-5xl py-4 my-2 font-extrabold text-black">NEW ARRIVALS</p>
           <p className="text-3xl font-bold text-green-500">Explore our diverse range, each piece a testament to unparalleled craftsmanship and timeless design, and find the perfect companion for your journey.
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -3,12 +3,13 @@ import axios from 'axios';
 
 const Products = () => {
   const [data, setData] = useState([]);
+  const [filter, setFilter] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
 
   useEffect(() => {
     const controller = new AbortController();
-    
+
     const fetchData = async () => {
       try {
         setLoading(true);
@@ -16,6 +17,7 @@ const Products = () => {
           signal: controller.signal,
         });
         setData(response.data);
+        setFilter(response.data);
       } catch (error) {
         if (axios.isCancel(error)) {
           console.log('Request canceled', error.message);
@@ -42,19 +44,56 @@ const Products = () => {
     return <div>Error loading data.</div>;
   }
 
+  // const Loading = () => {
+  //   return (
+  //     <>
+  //       Loading...
+  //     </>
+  //   );
+  // };
+
+  const ShowProducts = () => {
+    return (
+      <>
+        <div className="buttons d-flex justify-content-center mb-5 pb-5">
+          <button className="btn btn-outline-dark me-2">ALL</button>
+          <button className="btn btn-outline-dark me-2">LOUIS VUITTON</button>
+          <button className="btn btn-outline-dark me-2">GUCCI</button>
+          <button className="btn btn-outline-dark me-2">FENDI</button>
+          <button className="btn btn-outline-dark me-2">YSL</button>
+        </div>
+        {filter.map((product) => {
+          return (
+            <div className="col-md-3 mb-4">
+              <div className="card h-100 text-center p-4" key={product.id}>
+                <img src={product.image} className='card-img-top h-[250px]' height='250px' alt={product.name} />
+                <div className="card-body">
+                  <h5 className="card-title mb-0">{product.name.substring(0,12)}</h5>
+                  <p className="card-text">
+                    ${product.price}
+                  </p>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </>
+    );
+  };
+
   return (
     <>
-      <h1>Products</h1>
-      {data.map((post) => {
-        const { id, name, price, img } = post;
-        return (
-          <div className='card' key={id}>
-            <h2>{name}</h2>
-            <h1>{price}</h1>
-            <img src={img} alt={name} />
+      <div className='container my-5 py-5   '>
+        <div className="row">
+          <div className="col-12 mb-5">
+            <h1 className='display-6 fw-bolder text-center mb-5 pb-5'>Latest Products</h1>
+            <hr />
           </div>
-        );
-      })}
+        </div>
+        <div className="row justify-content-center">
+          {loading ? " " : <ShowProducts />}
+        </div>
+      </div>
     </>
   );
 };
